14dbf47cc2b6aece26a9b70b9ae4a79a4aee8a54
harshilAmbliya <harshilambliya15@gmail.com>
2023-11-29 13:09:37 +0530

    feat :Update page.js and remove page.module.css

---
+++ routes.js
@@ -0,0 +1,27 @@
+export const Routes = [
+    {
+        path: "/",
+        ele: <Home />,
+        Permissions: "",
+    },
+    {
+        path: "/about",
+        ele: <About />,
+        Permissions: "",
+    },
+    {
+        path: "/contact",
+        ele: <Contact />,
+        Permissions: "",
+    },
+    {
+        path: "/menu",
+        ele: <Menu />,
+        Permissions: "",
+    },
+    {
+        path: "/details",
+        ele: <Details />,
+        Permissions: "",
+    },
+]
